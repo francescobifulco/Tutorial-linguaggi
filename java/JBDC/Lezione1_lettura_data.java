41b25f9cbb97bf11ee77e9f1c54c338de5242e70
francescobifulco <francescobifulco362@gmail.com>
2026-05-01 17:10:52 +0200

    nuovo

--- java/JBDC/Lezione1_lettura_data.java
+++ java/JBDC/Lezione1_lettura_data.java
@@ -6,28 +6,29 @@ import java.sql.Statement;
 public class Lezione1_lettura_data {
     public static void main(String[] args) {
         // Dati di connessione
-        String url = "jdbc:mysql://localhost:3306/scuola_db";
-        String user = "root";
-        String password = "root";
+        final String DB_URL = "jdbc:mysql://localhost:3306/prova_db";
+        final String USER = "root";
+        final String PASS = "root";
+        final String QUERY = "SELECT id, first, last, age FROM Employees";
 
         // Usiamo il try-with-resources per chiudere automaticamente la connessione
-        try (Connection conn = DriverManager.getConnection(url, user, password);
-            Statement stmt = conn.createStatement()) {
+        try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            Statement stmt = conn.createStatement(); 
+            ResultSet rs = stmt.executeQuery(QUERY)){
             
             System.out.println("Connessione riuscita!");
 
-            String sql = "SELECT id, nome FROM studenti";
-            ResultSet rs = stmt.executeQuery(sql);
-
             // Iteriamo sui risultati
             while (rs.next()) {
-                int id = rs.getInt("id");
-                String nome = rs.getString("nome");
-                System.out.println("ID: " + id + ", Nome: " + nome);
+                System.out.print("ID: " + rs.getInt("id"));
+                System.out.print(", Age: " + rs.getInt("age"));
+                System.out.print(", First: " + rs.getString("first"));
+                System.out.println(", Last: " + rs.getString("last"));
             }
 
         } catch (Exception e) {
             System.out.println("Errore specifico: " + e.getMessage());
+            e.printStackTrace(); // Fondamentale per capire il problema
         }
     }
 }
