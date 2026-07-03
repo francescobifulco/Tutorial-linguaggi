4aa36ad40bcb7f35102436acce44d352976146a6
francescobifulco <francescobifulco362@gmail.com>
2026-07-03 17:37:42 +0200

    nuovo

---
+++ C#/Lezione C#/Lezione1_sintassi.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Lezione1_sintassi{
+    class Program{
+        static void Main(string[] args){
+            Console.WriteLine("Ciao mondo prova 1");
+        }
+    }
+}
