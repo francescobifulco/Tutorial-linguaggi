ee648cd8e8a16f32ec9abc5b564baeffa6c03937
francescobifulco <francescobifulco362@gmail.com>
2026-07-04 16:32:01 +0200

    nuovo

--- C#/obj/Debug/net10.0/C#.AssemblyInfo.cs
+++ C#/obj/Debug/net10.0/C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c898c230a369fa1f21c6359c9094fd987f2bfc87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c557459f808658a6f833e073b4db20906fa9f9c")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
