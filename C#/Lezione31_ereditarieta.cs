18ff796739aed2dcba704146d3bb1832a363a29b
francescobifulco <francescobifulco362@gmail.com>
2026-07-21 17:36:32 +0200

    nuovo

---
+++ C#/Lezione31_ereditarieta.cs
@@ -0,0 +1,11 @@
+using System;
+using Lezione31Class;
+
+namespace Lezione31 {
+    public class Lezione31_ereditarieta {
+        static void Main(string[] args){
+            Studente studente1 = new Studente("Luca", "Rossi", "4A");
+            Insegnante insegnante1 = new Insegnante("Anna", "Neri", "Matematica");
+        }
+    }
+}
