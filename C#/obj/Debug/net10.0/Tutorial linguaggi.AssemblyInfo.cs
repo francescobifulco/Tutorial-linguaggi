e5a54c47cfaf91cc7df5f8772bdc757e2d264a0e
francescobifulco <francescobifulco362@gmail.com>
2026-08-20 16:03:27 +0200

    nuovo

--- C#/obj/Debug/net10.0/Tutorial linguaggi.AssemblyInfo.cs
+++ C#/obj/Debug/net10.0/Tutorial linguaggi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tutorial linguaggi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2df61327a7514ec8b860d14b03ac84886d6a5ad2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e24ba2c79f617c9f244dc9b6c63caa40ac9c3a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tutorial linguaggi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tutorial linguaggi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
